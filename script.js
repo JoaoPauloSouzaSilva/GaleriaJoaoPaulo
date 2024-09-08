a75ebd2f84a035ce43d894c96ecb32afee7053dd
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-09-08 10:49:20 -0300

    Add modal story

--- script.js
+++ script.js
@@ -13,25 +13,44 @@ var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
 // Obter as imagens que abrem o modal
-var btns = document.querySelectorAll(".image-post");
+var post_btns = document.querySelectorAll(".image-post");
 
 // Obter a imagem dentro do modal que será trocada
-var modalImg = document.querySelector(".img-post-modal");
+var post_modalImg = document.querySelector(".img-post-modal");
 
 // Para cada imagem com a classe .image-post, adicionar evento de clique
-btns.forEach(function(btn) {
+post_btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
     // Pegar o src da imagem clicada
     var imgSrc = btn.getAttribute('src');
 
     // Alterar o src da imagem do modal para o da imagem clicada
-    modalImg.setAttribute('src', imgSrc);
+    post_modalImg.setAttribute('src', imgSrc);
 
     // Quando o usuário clicar na imagem, abre o modal
     modal.style.display = "block";
   });
 });
 
+// Obter as imagens que abrem o modal
+var story_btns = document.querySelectorAll(".image-story");
+
+// Obter a imagem dentro do modal que será trocada
+var story_modalImg = document.querySelector(".img-story-modal");
+
+// Para cada imagem com a classe .image-story, adicionar evento de clique
+story_btns.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    // Pegar o src da imagem clicada
+    var imgSrc = btn.getAttribute('src');
+
+    // Alterar o src da imagem do modal para o da imagem clicada
+    story_modalImg.setAttribute('src', imgSrc);
+
+    // Quando o usuário clicar na imagem, abre o modal
+    modal.style.display = "block";
+  });
+});
 
 // Quando o usuário clicar no <span> (x), fecha o modal
 span.onclick = function() {
